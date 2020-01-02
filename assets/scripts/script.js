318aff5e1ced4cbd93c19980ff5ccbec088c4fcb
Lynn O'Neill <loneill@afgaz.com>
2020-01-01 20:34:37 -0700

    Created html and resume

---
+++ assets/scripts/script.js
@@ -0,0 +1,8 @@
+// $(".btn").on("click", function() {
+//     alert("hello");
+//     let firstName = $("#name").text();
+//     alert("Thank you " + firstName);
+// })
+$("a").on("click", function () {
+    alert("hi");
+  })

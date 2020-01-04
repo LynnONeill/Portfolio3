3979033b68a58625611f6118466eb1e1c407795f
Lynn O'Neill <loneill@afgaz.com>
2020-01-03 20:21:03 -0700

    styling, added button functions with javascript

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -1,8 +1,22 @@
-// $(".btn").on("click", function() {
-//     alert("hello");
-//     let firstName = $("#name").text();
-//     alert("Thank you " + firstName);
-// })
-$("a").on("click", function () {
-    alert("hi");
-  })
+$( document ).ready(function() {
+    console.log( "ready!" );
+
+$("#submit").on("click", function() {
+
+    let name = $("#name").val();
+    let email = $("#email").val();
+    let subject = $("#subject").val();
+    let message = $("#message").val();
+    })
+
+
+$(".link").on("click", function() {
+    let link = $(this).attr("href");
+    window.location = link;
+})
+$(".inspect").on("click", function() {
+    let link = $(this).attr("href");
+    window.location = link;
+})
+
+});
